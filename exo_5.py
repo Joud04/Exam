9b4b282eb5e4a28e27c4bcdaa2a53eef11c8e4bb
Joud04 <88037139+Joud04@users.noreply.github.com>
2025-12-01 16:24:09 +0100

    Delete exo_5.py

--- exo_5.py
+++
@@ -1,5 +0,0 @@
-#Écrivez un programme Python pour trouver les nombres divisibles par 7 et multiples de 5, entre 1 500 et 2 700 (tous deux inclus).
-
-for nombre in range (1500, 2701): 
-    if (nombre % 7 == 0) and (nombre % 5 == 0):
-        print(nombre)
