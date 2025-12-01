9e42cc1023f6c7705db484edbea3dec9c1db77f8
Joud04 <88037139+Joud04@users.noreply.github.com>
2025-12-01 14:13:01 +0100

    Ajout de l'exo 3

---
+++ exo_3.py
@@ -0,0 +1,14 @@
+"""Écrivez un programme Python pour obtenir la série de Fibonacci entre 0 et 50.
+
+Remarque : La Suite de Fibonacci est la série de nombres :
+0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+Chaque nombre suivant est trouvé en additionnant les deux nombres qui le précèdent. """
+
+x = 0
+y = 1
+
+while x <= 50:
+    print(x)
+    temp = x
+    x = y 
+    y = temp + y
