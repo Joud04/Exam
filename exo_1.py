2d17ed86fae37ac888701bd5238bce4eaa4821de
Joud04 <88037139+Joud04@users.noreply.github.com>
2025-12-01 12:48:38 +0100

    Ajout de la fonction Exo 1

--- exo_1.py
+++ exo_1.py
@@ -1,1 +1,15 @@
-print("Hello world")
+# Écrivez une fonction Python qui prend une séquence de nombres et détermine si tous les nombres sont différents les uns des autres.
+
+def test_distinct(data) :
+    deja_vus = []
+
+    for nombre in data :
+        if nombre in deja_vus:
+            return False
+        
+        deja_vus.append(nombre)
+
+    return True
+
+print(test_distinct([1, 5, 7, 9]))
+print(test_distinct([2, 4, 5, 5, 7, 9]))
